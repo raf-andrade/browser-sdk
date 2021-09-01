9ec2fe880b69fbf02c1fb4129b8d459d1e566f52
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-09-01 17:21:05 +0200

    test

---
+++ scripts/test-commit.sh
@@ -0,0 +1,13 @@
+set -euxo pipefail
+
+git config --global user.email "ube+ci.web-ui@datadoghq.com"
+git config --global user.name "ci.web-ui"
+
+# add github public host key to the known_hosts
+mkdir -p ~/.ssh
+echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts
+
+date > hop
+git add hop
+git commit -m "update date"
+git push git@github.com:DataDog/web-ui.git HEAD
